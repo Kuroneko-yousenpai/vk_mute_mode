584d81516c337880c7b43bd457e55544dbd87a93
Ruri Gokou <Kuronekoyousenpai@gmail.com>
2024-06-15 02:49:05 +0300

    Improved users searching

--- vk_mute_mode.js
+++ vk_mute_mode.js
@@ -5,30 +5,30 @@
  * VK: https://vk.com/kuroneko_yousenpai
  */
 
-const userNames = ["Forget Me", "Евгений Стрелецкий", "Ilya Miroshnik", "Глеб Алмазов", "Влад Цветков"];
+// List of vk user IDs to check
+const userIDs = ["260910699", "864676605", "667817175", "647474124", "795917421"];
 
 function checkAndRemoveElements() {
-    const messageElements = document.querySelectorAll('.im-mess-stack._im_mess_stack');
+    var messageElements = document.querySelectorAll('.im-mess-stack._im_mess_stack');
     
     messageElements.forEach((messageElement) => {
-        // Find the <a> tag within the current message element
-        const linkElement = messageElement.querySelector('.im-mess-stack--lnk');
+        var userId = messageElement.getAttribute('data-peer');
         
-        // Check if the <a> tag exists and if its text content matches any name in the userNames list - so remove
-        if (linkElement && userNames.includes(linkElement.textContent.trim())) {
+        // Check if the data-peer attribute matches any ID in the userIDs list
+        if (userId && userIDs.includes(userId)) {
             messageElement.remove();
         }
     });
 }
 
 // Set up a MutationObserver to watch for changes in the target container
-const targetNode = document.querySelector('._im_peer_history.im-page-chat-contain.glubs-container');
+var targetNode = document.querySelector('._im_peer_history.im-page-chat-contain.glubs-container');
 
-// Observer configuration: watch for child node additions
-const config = { childList: true, subtree: true };
+// Observer configuration
+var config = { childList: true, subtree: true };
 
 // Callback function to execute when mutations are observed
-const callback = function(mutationsList, observer) {
+var callback = function(mutationsList, observer) {
     for(const mutation of mutationsList) {
         if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
             checkAndRemoveElements();
@@ -36,6 +36,6 @@ const callback = function(mutationsList, observer) {
     }
 };
 
-const observer = new MutationObserver(callback);
+var observer = new MutationObserver(callback);
 observer.observe(targetNode, config);
 checkAndRemoveElements();
