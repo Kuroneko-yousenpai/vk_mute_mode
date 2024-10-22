549da90d4b6c6d19961074a8b058766b1265f8aa
Kuroneko-yousenpai <49292348+Kuroneko-yousenpai@users.noreply.github.com>
2024-10-23 01:44:22 +0300

    Improved log like in cs go hvh

--- vk_mute_mode.js
+++ vk_mute_mode.js
@@ -8,6 +8,14 @@
 // List of vk user IDs to check
 const userIDs = ["260910699", "864676605", "667817175", "647474124", "795917421", "801639527", "631760048", "847428438", "864978588", "842568982", "138277688", "593940366", "344974545", "851942133", "116376805", "299195182", "816991698", "140496489", "749942771", "587824212", "549905143", "706014098", "544632435", "550475212", "121883329", "518913444", "293484839", "286084044", "227149446", "227598857", "712822989", "604107214", "581457203", "437798198", "182074336", "815276980", "447761967", "411352286", "333040068", "270252048", "532977930", "613964770", "514885220", "825611655", "508794646", "223749618", "381493022", "867964351", "836906395", "882379475", "873485812", "201359434", "387375926", "631436327", "774779548", "708786845", "533914378", "70871095", "582237092", "425342592", "829580079", "533920231", "459146091", "877887711", "639643509", "863010424"];
 
+function getCurrentTime() {
+  let now = new Date();
+  let hours = String(now.getHours()).padStart(2, '0');
+  let minutes = String(now.getMinutes()).padStart(2, '0');
+  let seconds = String(now.getSeconds()).padStart(2, '0');
+  return `${hours}:${minutes}:${seconds}`;
+}
+
 function checkAndRemoveElements() {
     const messageElements = document.querySelectorAll('.im-mess-stack._im_mess_stack');
     
@@ -21,7 +29,8 @@ function checkAndRemoveElements() {
             let userName = nameElement ? nameElement.textContent.trim() : null;
 			let displayName = userName ? userName : userId;
 			hp = Math.floor(Math.random() * (250 - 150 + 1)) + 150
-			console.log(`[consollite] Hurt ${displayName} in head for ${hp} hp (0 hp remaining)`)
+			currTime = getCurrentTime()
+			console.log(`[${currTime}] [consollite] Hurt ${displayName} in head for ${hp} hp (0 hp remaining)`)
 			messageElement.remove();
         }
     });
